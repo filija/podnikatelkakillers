8fb60c6093db771f226474db2fdadf47a6c25f4e
Croolman <croolman@gmail.com>
2015-10-25 10:36:29 +0100

    Shell-sort, functions, etc.

---
+++ ial.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <string.h>
+
+int boyer_moore(char*, char*);
+int length(char*);
+char *substr(char *, int, int); 
+char *sort(char *);
+char *concat(char *, char *);
